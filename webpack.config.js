7c1297d2f444c114739945a46b955ec57cc31c50
Zhuyunfeng <i@zhuyunfeng.com>
2018-05-04 14:21:38 +0800

    更新到第四章

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
 const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -10,7 +10,7 @@ const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -28,23 +28,8 @@ const config = {
                 use: [
                     'style-loader',
                     'css-loader'
-
                 ]
-            },
-            {
-                test: /.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
+            },            
             {
                 test: /\.(gif|jpg|png|svg)$/,
                 use: [
@@ -70,6 +55,20 @@ const config = {
 }
 
 if (isDev) {
+    config.module.rules.push({
+        test: /.styl$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true,
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool='#cheap-module-eval-source-map'
     config.devServer ={
         port: 8000,
@@ -83,6 +82,37 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{//生产环境
+    config.entry={
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename='[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /.styl$/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [                
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'runtime'
+        })
     )
 }
 
