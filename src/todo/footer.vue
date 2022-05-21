8efe3bf48e3d6bc7ae3c3583917e6547c2acb242
lisa <i@zhuyunfeng.com>
2022-05-21 22:33:28 +0800

    feat: change to vite

---
+++ src/todo/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="footer">
+        <span>Written by {{author}}</span>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            author: 'Fyzhu'
+        }
+    }
+}
+</script>
+<style lang="stylus">
+
+@import "../assets/styles/footer.styl";
+
+</style>
